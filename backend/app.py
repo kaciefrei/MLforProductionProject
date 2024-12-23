41bd6ed02e67287c3533e320aa2f716fa322d251
kaciefrei <kaci.bouaouli@efrei.net>
2024-12-23 10:24:00 +0100

    test deployment RailWay.app

--- backend/app.py
+++ backend/app.py
@@ -7,12 +7,8 @@ import os
 # Initialisation de l'application Flask
 app = Flask(__name__)
 
-CORS(app, resources={r"/predict": {
-    "origins": "https://forest-fire-frontend-production.up.railway.app",
-    "methods": ["GET", "POST", "OPTIONS"],
-    "allow_headers": ["Content-Type", "Authorization", "Accept"]
-}})
-
+# Autoriser les CORS pour tous les domaines (ou spécifiez votre domaine frontend)
+CORS(app, resources={r"/predict": {"origins": "https://forest-fire-frontend-production.up.railway.app"}})
 
 # Charger le modèle TFLite
 interpreter = tflite.Interpreter(model_path="models/forest_fire_model.tflite")
@@ -24,14 +20,9 @@ output_details = interpreter.get_output_details()
 
 # Fonction de prédiction
 def predict(features):
-    # Mettre en forme les données pour l'entrée
     features = np.array(features, dtype=np.float32).reshape(input_details[0]['shape'])
-    
-    # Passer les données au modèle
     interpreter.set_tensor(input_details[0]['index'], features)
     interpreter.invoke()
-
-    # Récupérer le résultat
     prediction = interpreter.get_tensor(output_details[0]['index'])
     return prediction
 
@@ -39,8 +30,16 @@ def predict(features):
 def home():
     return "Algerian Forest Fires Prediction API is running!"
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST", "OPTIONS"])
 def predict_route():
+    if request.method == "OPTIONS":
+        # Répondre à la requête OPTIONS avec les en-têtes CORS appropriés
+        response = jsonify({"message": "CORS preflight"})
+        response.headers.add('Access-Control-Allow-Origin', 'https://forest-fire-frontend-production.up.railway.app')
+        response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
+        return response
+
     try:
         # Récupérer les données du formulaire (JSON envoyé par le frontend)
         data = request.json
@@ -68,14 +67,6 @@ def predict_route():
 
     except Exception as e:
         return jsonify({"error": str(e)})
-
-@app.route('/predict', methods=["OPTIONS"])
-def options():
-    response = jsonify({'message': 'OK'})
-    response.headers.add('Access-Control-Allow-Origin', 'https://forest-fire-frontend-production.up.railway.app')
-    response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
-    return response
 
 
 if __name__ == "__main__":
