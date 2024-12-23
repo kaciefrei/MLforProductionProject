ac9566ee039285ab90e1b91ebe0f5065f92b29b6
kaciefrei <kaci.bouaouli@efrei.net>
2024-12-23 10:14:57 +0100

    test deployment RailWay.app

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,12 @@ import os
 # Initialisation de l'application Flask
 app = Flask(__name__)
 
-CORS(app, resources={r"/predict": {"origins": "*"}})
+CORS(app, resources={r"/predict": {
+    "origins": "https://forest-fire-frontend-production.up.railway.app",
+    "methods": ["GET", "POST", "OPTIONS"],
+    "allow_headers": ["Content-Type", "Authorization", "Accept"]
+}})
+
 
 # Charger le modèle TFLite
 interpreter = tflite.Interpreter(model_path="models/forest_fire_model.tflite")
@@ -63,6 +68,14 @@ def predict_route():
 
     except Exception as e:
         return jsonify({"error": str(e)})
+
+@app.route('/predict', methods=["OPTIONS"])
+def options():
+    response = jsonify({'message': 'OK'})
+    response.headers.add('Access-Control-Allow-Origin', 'https://forest-fire-frontend-production.up.railway.app')
+    response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept')
+    return response
 
 
 if __name__ == "__main__":
